93adf59635723c4e6a46af35fc960ab4ca346c5f
Bill Chan <biaocy91@gmail.com>
2018-12-16 17:17:30 +0800

    Lession 8

---
+++ L8/helloworld-args.asm
@@ -0,0 +1,22 @@
+; Hello World Program (Passing argument from the command line)
+; make run
+
+%include		'functions.asm'
+
+SECTION	.text
+global	_start
+
+_start:
+
+	pop		ecx
+
+nextArg:
+	cmp		ecx, 0h
+	jz		noMoreArgs
+	pop		eax
+	call	sprintLF
+	dec		ecx
+	jmp		nextArg
+
+noMoreArgs:
+	call	quit
