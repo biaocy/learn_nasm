8081384c432c086331ab467e39f922668d006626
Bill Chan <biaocy91@gmail.com>
2018-12-16 23:49:09 +0800

    Lession 16

---
+++ L16/calculator-atoi.asm
@@ -0,0 +1,26 @@
+; Calculator (ATOI)
+; make run
+
+%include		'functions.asm'
+
+SECTION	.text
+global	_start
+
+_start:
+
+	pop		ecx
+	mov		edx, 0
+
+nextArg:
+	cmp		ecx, 0h
+	jz		noMoreArgs
+	pop		eax
+	call	atoi
+	add		edx, eax
+	dec		ecx
+	jmp		nextArg
+
+noMoreArgs:
+	mov		eax, edx
+	call	iprintLF
+	call	quit
