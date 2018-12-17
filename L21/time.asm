7011a81d095b94a050ea525d2f5a30c6aa136dd4
Bill Chan <biaocy91@gmail.com>
2018-12-17 23:23:27 +0800

    Lession 21

---
+++ L21/time.asm
@@ -0,0 +1,21 @@
+; Time
+; make run
+
+%include		'functions.asm'
+
+SECTION	.data
+msg			db		'Seconds since Jan 01 1970: ', 0h
+
+SECTION	.text
+global	_start
+
+_start:
+
+	mov		eax, msg
+	call	sprint
+
+	mov		eax, 13
+	int		80h
+
+	call	iprintLF
+	call	quit
